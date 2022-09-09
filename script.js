4c73a81c2d916d44813f88127ad47bcfa397add9
charlie-daniels <charlied030401@gmail.com>
2022-09-09 14:17:08 +0100

    Refactor

--- script.js
+++ script.js
@@ -1,65 +1,75 @@
-const gameBoard = (function () {
-  let _board = [];
-  let _isFinished = false;
-  const reset = () => _board = new Array(9).fill('');
-  const isTileEmpty = (index) => {
-    if (_board[index] === '') return true;
-    return false;
-  }
-  const setTile = (tile, player) => {
-    _board[tile] = player;
-  }
-  return {
-    reset,
-    isTileEmpty,
-    setTile
-  }
-})();
 const displayController = (function (doc) {
-  let currentPlayer = 'X';
-
-  const _switchPlayer = (currentPlayer) => {
-    if (currentPlayer === 'X') return 'O';
-    return 'X';
+  const _addTileListeners = (performClick) => {
+    const tiles = doc.querySelectorAll(`#board .tile`);
+    tiles.forEach(t => {
+      t.addEventListener('click', (e) => performClick(e.target.getAttribute('data-index')));
+    });
   }
-  const _updateBoard = (move, player) => {
+
+  const updateBoard = (move, player) => {
     const tile = doc.querySelector(`#board>[data-index='${move}']`);
     tile.textContent = player;
   }
+  const init = (performClick) => {
+    _addTileListeners(performClick);
+  }
+  return {
+    updateBoard,
+    init
+  }
+})(document);
+
+const gameBoard = (function () {
+  let _board = [];
+  let _currentPlayer = 'X'
+
+  const _reset = () => {
+    _board = new Array(9).fill('');
+  }
+  const _switchPlayer = (_currentPlayer) => {
+    if (_currentPlayer === 'X') return 'O';
+    return 'X';
+  }
+  const _isTileEmpty = (index) => {
+    if (_board[index] !== 'X' || _board[index] !== 'O') return true;
+    return false;
+  }
+  const _setTile = (tile, player) => {
+    _board[tile] = player;
+  }
 
   const playRound = (nextPlayerMove) => {
-    if (gameBoard.isTileEmpty(nextPlayerMove)) gameBoard.setTile(nextPlayerMove, currentPlayer);
+    if (_isTileEmpty(nextPlayerMove)) _setTile(nextPlayerMove, _currentPlayer);
     else return null; // Maybe add retry variable here?
-    _updateBoard(nextPlayerMove, currentPlayer);
-    console.log(currentPlayer);
-    currentPlayer = _switchPlayer(currentPlayer);
-  }
-  const assignTiles = () => {
-    const tiles = doc.querySelectorAll(`#board .tile`);
-    tiles.forEach(t => {
-      t.addEventListener('click', (e) => playRound(e.target.getAttribute('data-index')));
-    });
+    displayController.updateBoard(nextPlayerMove, _currentPlayer);
+    // let result = _checkWinner();
+    // if (result != null) return;
+    _currentPlayer = _switchPlayer(_currentPlayer);
   }
   const newGame = (player1, player2) => {
     player1.reset();
     player2.reset();
-    gameBoard.reset();
-    assignTiles();
+    _reset();
+    displayController.init(playRound);
   }
+
   return {
-    newGame,
-    playRound
+    playRound,
+    newGame
   }
-})(document);
+})();
+
 const Player = () => {
   let _score = 0;
   let _totalScore = 0;
+  
   const getScore = () => _score;
   const getTotalScore = () => _totalScore;
   const win = () => _score++;
   const reset = () => {
     _score = 0;
   }
+
   return {
     getScore,
     getTotalScore,
@@ -67,6 +77,7 @@ const Player = () => {
     reset
   };
 } 
+
 const Computer = () => {
   const logic = () => {
     // ai code
@@ -75,7 +86,9 @@ const Computer = () => {
   return Object.assign({}, protoPlayer, logic)
 }
 
+function newGame() {
+  let player1 = Player(); let player2 = Player();
+  gameBoard.newGame(player1, player2);
+}
 
-
-let player1 = Player(); let player2 = Player();
-displayController.newGame(player1, player2);
+newGame();
